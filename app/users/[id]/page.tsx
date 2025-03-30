ab5c8e63812c0d9085a739204cf9d49e7fd60471
manueltuor <manuel_tuor@bluewin.ch>
2025-03-30 16:13:09 +0200

    started profile page, added sidebar

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,23 +1,18 @@
-// your code here for S2 to display a single user profile after having clicked on it
-// each user has their own slug /[id] (/1, /2, /3, ...) and is displayed using this file
-// try to leverage the component library from antd by utilizing "Card" to display the individual user
-// import { Card } from "antd"; // similar to /app/users/page.tsx
-
 "use client";
-// For components that need React hooks and browser APIs,
-// SSR (server side rendering) has to be disabled.
-// Read more here: https://nextjs.org/docs/pages/building-your-application/rendering/server-side-rendering
 
 import React from "react";
 
-const Profile: React.FC = () => {
-  return (
-    <div className="card-container">
-      <p>
-        <strong>SampleUser</strong>
-      </p>
-    </div>
-  );
-};
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/ui/app-sidebar"
 
-export default Profile;
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  )
+}
