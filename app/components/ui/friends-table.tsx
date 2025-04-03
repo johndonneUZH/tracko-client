3dceb6e2f2ff7628b1f734dc0a37ee0679c8fa6e
manueltuor <manuel_tuor@bluewin.ch>
2025-04-03 15:53:03 +0200

    small change

--- app/components/ui/friends-table.tsx
+++ app/components/ui/friends-table.tsx
@@ -11,24 +11,24 @@ export function FriendsTable() {
         <table className="w-full">
             <tbody>
                 <tr className="border-b border-gray-300 even:bg-gray-100">
-                    <td className="px-4 py-2 text-left">
+                    <td className="px-2 py-1 text-left w-1">
                         <Avatar className="h-8 w-8 rounded-lg">
                             <AvatarImage src={"https://avatar.vercel.sh/jill"} />
                         </Avatar>
                     </td>
-                    <td className="px-4 py-2 text-left">Mark Zuckerberg</td>
-                    <td className="px-4 py-2 text-left">
+                    <td className="px-2 py-1 text-left w-full">Mark Zuckerberg</td>
+                    <td className="px-2 py-1 text-right w-1">
                         <span className="h-3 w-3 rounded-full bg-green-500 inline-block"></span>
                     </td>
                 </tr>
                 <tr className="border-b border-gray-300 even:bg-gray-100">
-                    <td className="px-4 py-2 text-left">
+                    <td className="px-2 py-1 text-left w-1">
                         <Avatar className="h-8 w-8 rounded-lg">
                             <AvatarImage src={"https://avatar.vercel.sh/jane"} />
                         </Avatar>
                     </td>
-                    <td className="px-4 py-2 text-left">Elon Musk</td>
-                    <td className="px-4 py-2 text-left">
+                    <td className="px-2 py-1 text-left w-full">Elon Musk</td>
+                    <td className="px-2 py-1 text-right w-1">
                         <span className="h-3 w-3 rounded-full bg-red-500 inline-block"></span>
                     </td>
                 </tr>
