9329b7804e8e2f6c3149a3c5a3cf6139ab6aac43
fabiotilor <fabio.dimeo13@gmail.com>
2025-04-10 14:21:35 +0200

    Found more files that were impacted by parsing bug

--- app/components/user_page/projects-table.tsx
+++ app/components/user_page/projects-table.tsx
@@ -19,31 +19,19 @@ export function ProjectsTable() {
                 </tr>
             </thead>
             <tbody>
-<<<<<<<< HEAD:app/components/ui/profile/projects-table.tsx
-                <tr className="even:bg-gray-100">
-========
                 <tr className="bg-gray-100 even:bg-transparent">
->>>>>>>> 49cc0026cd237939ab54eae25965739b2c8cd1d7:app/components/user_page/projects-table.tsx
                     <td className="px-4 py-2 text-left"><University/></td>
                     <td className="px-4 py-2 text-left">Sopra</td>
                     <td className="px-4 py-2 text-left">Contributor</td>
                     <td className="px-4 py-2 text-left">11</td>
                 </tr>
-<<<<<<<< HEAD:app/components/ui/profile/projects-table.tsx
-                <tr className="even:bg-gray-100">
-========
                 <tr className="bg-gray-100 even:bg-transparent">
->>>>>>>> 49cc0026cd237939ab54eae25965739b2c8cd1d7:app/components/user_page/projects-table.tsx
                     <td className="px-4 py-2 text-left"><Brain/></td>
                     <td className="px-4 py-2 text-left">Tracko</td>
                     <td className="px-4 py-2 text-left">Admin</td>
                     <td className="px-4 py-2 text-left">5</td>
                 </tr>
-<<<<<<<< HEAD:app/components/ui/profile/projects-table.tsx
-                <tr className="even:bg-gray-100">
-========
                 <tr className="bg-gray-100 even:bg-transparent">
->>>>>>>> 49cc0026cd237939ab54eae25965739b2c8cd1d7:app/components/user_page/projects-table.tsx
                     <td className="px-4 py-2 text-left"><Apple/></td>
                     <td className="px-4 py-2 text-left">Apple</td>
                     <td className="px-4 py-2 text-left">Contributor</td>
