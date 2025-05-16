6e69177d6b79455b43e00cd101a51c17c711b05a
manueltuor <manuel_tuor@bluewin.ch>
2025-05-16 13:52:06 +0200

    edit fix

--- app/components/dashboard_Project/IdeaModal.tsx
+++ app/components/dashboard_Project/IdeaModal.tsx
@@ -47,6 +47,7 @@ interface IdeaModalProps {
   onLogComment?: (action: string, title: string) => void;
   commentMap: Record<string, Comment>;
   members: User[];
+  alreadyOpenSub: boolean;
 }
 
 export default function IdeaModal({
@@ -61,11 +62,12 @@ export default function IdeaModal({
   onLogComment,
   commentMap,
   members,
+  alreadyOpenSub,
 }: IdeaModalProps) {
   const [title, setTitle] = useState(idea.ideaName || "");
   const [body, setBody] = useState(idea.ideaDescription || "");
   const [isOpen, setIsOpen] = useState(true);
-  const [isOpenSub, setIsOpenSub] = useState(false);
+  const [isOpenSub, setIsOpenSub] = useState(alreadyOpenSub);
 
   const hasChanges = title !== idea.ideaName || body !== idea.ideaDescription;
   const canEditFr = canEdit || (idea.ownerId === currentUserId)
@@ -149,7 +151,7 @@ export default function IdeaModal({
                   <Input
                     value={title}
                     onChange={(e) => setTitle(e.target.value)}
-                    disabled={!canEdit}
+                    disabled={!canEditFr}
                     className="mt-1 overflow-auto whitespace-nowrap"
                     placeholder="Title"
                   />
@@ -157,7 +159,7 @@ export default function IdeaModal({
                   <Textarea
                     value={body}
                     onChange={(e) => setBody(e.target.value)}
-                    disabled={!canEdit}
+                    disabled={!canEditFr}
                     className="mt-1 min-h-60"
                     placeholder="Description"
                   />
@@ -168,7 +170,7 @@ export default function IdeaModal({
                     onSave(title, body)
                     setIsOpenSub(false)
                   }}
-                    disabled={!canEdit}>
+                    disabled={!canEditFr}>
                     <Save className="w-4 h-4 mr-1" />
                     Save
                   </Button>
